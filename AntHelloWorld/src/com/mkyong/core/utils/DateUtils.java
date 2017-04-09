6053918277c27233879226ff753a08f59e8fb36a
asudheerkumar89 <sudheer9.sr@gmail.com>
2017-04-09 18:57:00 +0530

    i am committing

---
+++ AntHelloWorld/src/com/mkyong/core/utils/DateUtils.java
@@ -0,0 +1,12 @@
+package com.mkyong.core.utils;
+
+public class DateUtils {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+
+	}
+
+}
